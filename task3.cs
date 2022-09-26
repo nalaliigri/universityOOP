fd9f3fd86336a6a272b28b995634412483e5eaa5
nalaliigri <103165792+nalaliigri@users.noreply.github.com>
2022-09-26 10:38:15 +0300

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace task3
+{
+    class Converter
+    {
+        private decimal _dollarToUAH;
+        private decimal _euroToUAH;
+        public Converter(decimal dollarToUAH, decimal euroToUAH)
+        {
+            _dollarToUAH = dollarToUAH;
+            _euroToUAH = euroToUAH;
+        }
+
+        public void dollarsToUAH(decimal dollars)
+        {
+            Console.WriteLine(dollars * _dollarToUAH);
+        }
+
+        public void eurosToUAH(decimal euros)
+        {
+            Console.WriteLine(euros * _euroToUAH);
+        }
+
+        public void uahToDollars(decimal UAH)
+        {
+            Console.WriteLine(UAH / _dollarToUAH);
+        }
+
+        public void uahToEuros(decimal UAH)
+        {
+            Console.WriteLine(UAH / _euroToUAH);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(37.63M, 36.29M);
+            Console.WriteLine("USD to UAH:");
+            decimal money = Convert.ToDecimal(Console.ReadLine());
+            converter.dollarsToUAH(money);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("UAH to USD:");
+            money = Convert.ToDecimal(Console.ReadLine());
+            converter.uahToDollars(money);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("EURO to UAH:");
+            money = Convert.ToDecimal(Console.ReadLine());
+            converter.eurosToUAH(money);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("UAH to EURO:");
+            money = Convert.ToDecimal(Console.ReadLine());
+            converter.uahToEuros(money);
+            Console.WriteLine(" ");
+        }
+    }
+}
